eb509330d2a70b0cb5ea549be612968fb51ca6be
adesh1167 <54195460+adesh1167@users.noreply.github.com>
2025-11-30 13:07:32 +0100

    Migrate backend to api.gsportstade.com

--- src/data/url.js
+++ src/data/url.js
@@ -1,1 +1,2 @@
-export const baseApiUrl = "https://cywavesinvestment.com/gst/handlers"
+// export const baseApiUrl = "https://cywavesinvestment.com/gst/handlers";
+export const baseApiUrl = "https://api.gsportstrade.com/handlers";
